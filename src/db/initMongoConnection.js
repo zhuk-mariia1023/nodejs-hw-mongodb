642402638683951eb4f4918d9c3417a3185b1cf5
Mariia Zhuk <mariia.zhuk1023@gmail.com>
2025-08-12 18:48:44 +0300

    corrected initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 import { getEnvVar } from '../utils/getEnvVar.js';
 
-export const initMongoDB = async () => {
+export const initMongoConnection = async () => {
   try {
     const user = getEnvVar('MONGODB_USER');
     const pwd = getEnvVar('MONGODB_PASSWORD');
