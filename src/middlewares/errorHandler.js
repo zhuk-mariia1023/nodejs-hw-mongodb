087311911341726b374be29185f26e58832850ad
Mariia Zhuk <mariia.zhuk1023@gmail.com>
2025-08-26 23:28:22 +0300

    fixed errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,16 +1,29 @@
-import { HttpError } from 'http-errors';
-
 export const errorHandler = (err, req, res, next) => {
-  if (err instanceof HttpError) {
-    res.status(err.status).json({
-      status: err.status,
-      message: err.message || 'Error',
-      data: err.message,
+  if (err.isJoi) {
+    return res.status(400).json({
+      status: 400,
+      message: 'BadRequestError',
+      data: {
+        message: 'Bad request',
+        errors: err.details.map((e) => ({
+          message: e.message,
+          path: e.path,
+          type: e.type,
+          context: e.context,
+        })),
+      },
     });
-    return;
   }
 
-  res.status(500).json({
+  if (err.status && err.expose) {
+    return res.status(err.status).json({
+      status: err.status,
+      message: err.message || 'Error',
+      data: err.data || err.message,
+    });
+  }
+
+  return res.status(500).json({
     status: 500,
     message: 'Something went wrong',
     data: err.message,
