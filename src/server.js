aa9af9a5ff7a3c53eac63351336e04f9c6d621fc
Mariia Zhuk <mariia.zhuk1023@gmail.com>
2025-09-05 13:13:38 +0300

    change server.js

--- src/server.js
+++ src/server.js
@@ -25,13 +25,13 @@ export const setupServer = () => {
     }),
   );
 
+  app.use('/api-docs', swaggerDocs());
+
   app.use(router);
 
   app.use(notFoundHandler);
 
   app.use(errorHandler);
-
-  app.use('/api-docs', swaggerDocs());
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
